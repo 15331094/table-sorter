620678843d7b98a8afd194a851e21fbd5cba9710
一条咸鱼的自我修养 <1617899539@qq.com>
2016-11-01 01:55:26 +0800

    Update table.js

--- table.js
+++ table.js
@@ -9,25 +9,30 @@ $(document).ready(function() {
     //记录表头被点击是第几次，up表示点击的第一次，down第二次
     var what_times = "up", when_times = "up", location_times = "up";
     var fName_times = "up", lName_times = "up", check_times = "up";
+    /*
     for(var i = 0; i < 3; i++) {
         //把每一列的数据放进数组中
-        what[i] = $("#todo tbody tr:eq(" + i + ") td:eq(0)").text();
-        when[i] = $("#todo tbody tr:eq(" + i + ") td:eq(1)").text();
-        location[i] = $("#todo tbody tr:eq(" + i + ") td:eq(2)").text();
-        firstName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(0)").text();
-        lastName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(1)").text();
-        checkIn[i] = $("#staff tbody tr:eq(" + i + ") td:eq(2)").text();        
+        what[i] = $("#todo tbody tr:eq(" + i + ") td:eq(0) input").val();
+        when[i] = $("#todo tbody tr:eq(" + i + ") td:eq(1) input").val();
+        location[i] = $("#todo tbody tr:eq(" + i + ") td:eq(2) input").val();
+        firstName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(0) input").val();
+        lastName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(1) input").val();
+        checkIn[i] = $("#staff tbody tr:eq(" + i + ") td:eq(2) input").val();        
+    }*/
+
+    for(var i = 0; i < 3; i++) {
         //给每一个表头末尾增加一个div三角形
         $("#todo thead tr:eq(0) th:eq(" + i + ")").append($("<div></div>"));
-        $("#staff thead tr:eq(0) th:eq(" + i + ")").append($("<div></div>"));
+        $("#staff thead tr:eq(0) th:eq(" + i + ")").append($("<div></div>"));        
     }
+/*
     //给每一列数据备份
     var whatStandard = [].concat(what);
     var whenStandard = [].concat(when);
     var locationStandard = [].concat(location);
     var firstNameStandard = [].concat(firstName);
     var lastNameStandard = [].concat(lastName);
-    var checkInStandard = [].concat(checkIn);
+    var checkInStandard = [].concat(checkIn);*/
     //对应交换数组的第j位和j + 1位，exchange()函数在sort函数中被调用
     function exchange(table, j) {
         if(table == 0) {
@@ -57,12 +62,35 @@ $(document).ready(function() {
             checkIn[j + 1] = tempcheck;             
         }
     }
+
+    function reget() {
+        for(var i = 0; i < 3; i++) {
+        //把每一列的数据放进数组中
+            what[i] = $("#todo tbody tr:eq(" + i + ") td:eq(0) input").val();
+            when[i] = $("#todo tbody tr:eq(" + i + ") td:eq(1) input").val();
+            location[i] = $("#todo tbody tr:eq(" + i + ") td:eq(2) input").val();
+            firstName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(0) input").val();
+            lastName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(1) input").val();
+            checkIn[i] = $("#staff tbody tr:eq(" + i + ") td:eq(2) input").val();             
+        }
+        //给每一列数据备份
+        whatStandard = [].concat(what);
+        whenStandard = [].concat(when);
+        locationStandard = [].concat(location);
+        firstNameStandard = [].concat(firstName);
+        lastNameStandard = [].concat(lastName);
+        checkInStandard = [].concat(checkIn); 
+        
+        //alert(when[0] + " " + when[1] + " " + when[2]);
+             
+    }
     /*
     对数组进行排序，table为数字，0代表todo表，1代表staff表
     th代表每个表的第几列，0代表第一列，以此类推，order是字符串
     increase代表升序，decrease代表降序
     */
     function sort(table, th, order) {
+
         what = [].concat(whatStandard);
         when = [].concat(whenStandard);
         location = [].concat(locationStandard);
@@ -70,7 +98,16 @@ $(document).ready(function() {
         firstName = [].concat(firstNameStandard);
         lastName = [].concat(lastNameStandard);
         checkIn = [].concat(checkInStandard);
-
+/*
+        for(var i = 0; i < 3; i++) {
+        //把每一列的数据放进数组中
+            what[i] = $("#todo tbody tr:eq(" + i + ") td:eq(0) input").val();
+            when[i] = $("#todo tbody tr:eq(" + i + ") td:eq(1) input").val();
+            location[i] = $("#todo tbody tr:eq(" + i + ") td:eq(2) input").val();
+            firstName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(0) input").val();
+            lastName[i] = $("#staff tbody tr:eq(" + i + ") td:eq(1) input").val();
+            checkIn[i] = $("#staff tbody tr:eq(" + i + ") td:eq(2) input").val();             
+        }*/
         var temp;
         if(th == 0) temp = [].concat(what);
         if(th == 1) temp = [].concat(when);
@@ -95,6 +132,7 @@ $(document).ready(function() {
             for(var i = 0; i < 2; i++) {
                 for(var j = 0; j < 2 - i; j++) {
                     if(temp[j] < temp[j + 1]) {
+                        var t = temp[j];
                         temp[j] = temp[j + 1];
                         temp[j + 1] = t;
                         exchange(table, j);
@@ -102,18 +140,19 @@ $(document).ready(function() {
                 }
             }    
         }
+        //alert(when[0] + " " + when[1] + " " + when[2]);
         reset();
     }
     //在进行排序之后reset()函数把数组的数据重新放回表格中
     function reset() {
         for(var i = 0; i < 3; i++) {
-            $("#todo tbody tr:eq(" + i + ") td:eq(0)").text(what[i]);
-            $("#todo tbody tr:eq(" + i + ") td:eq(1)").text(when[i]);
-            $("#todo tbody tr:eq(" + i + ") td:eq(2)").text(location[i]);
+            $("#todo tbody tr:eq(" + i + ") td:eq(0) input").val(what[i]);
+            $("#todo tbody tr:eq(" + i + ") td:eq(1) input").val(when[i]);
+            $("#todo tbody tr:eq(" + i + ") td:eq(2) input").val(location[i]);
 
-            $("#staff tbody tr:eq(" + i + ") td:eq(0)").text(firstName[i]);
-            $("#staff tbody tr:eq(" + i + ") td:eq(1)").text(lastName[i]);
-            $("#staff tbody tr:eq(" + i + ") td:eq(2)").text(checkIn[i]);  
+            $("#staff tbody tr:eq(" + i + ") td:eq(0) input").val(firstName[i]);
+            $("#staff tbody tr:eq(" + i + ") td:eq(1) input").val(lastName[i]);
+            $("#staff tbody tr:eq(" + i + ") td:eq(2) input").val(checkIn[i]);  
         }
     }
     /*change()函数在点击表头和排序完成之后对表头的样式进行修改，
@@ -159,6 +198,7 @@ $(document).ready(function() {
 //按照what来进行排序
     $("#todo thead tr:eq(0) th:eq(0)").click(function() {
        // alert("what");
+        reget();
         if(what_times == "up") {
             sort(0, 0, "increase");
             change("todo", 0, "increase");
@@ -172,7 +212,7 @@ $(document).ready(function() {
     });
 //按照when来进行排序
     $("#todo thead tr:eq(0) th:eq(1)").click(function() {
-
+        reget();
         if(when_times == "up") {
             sort(0, 1, "increase");
             change("todo", 1, "increase");
@@ -186,6 +226,7 @@ $(document).ready(function() {
     });
 //按照location来进行排序
     $("#todo thead tr:eq(0) th:eq(2)").click(function() {
+        reget();
         if(location_times == "up") {
             sort(0, 2, "increase");
             change("todo", 2, "increase");
@@ -199,6 +240,7 @@ $(document).ready(function() {
     });
 //按照firsname来进行排序
     $("#staff thead tr:eq(0) th:eq(0)").click(function() {
+        reget();
         if(fName_times == "up") {
             sort(1, 3, "increase");
             change("staff", 0, "increase");
@@ -212,6 +254,7 @@ $(document).ready(function() {
     });
 //按照lastname来进行排序
     $("#staff thead tr:eq(0) th:eq(1)").click(function() {
+        reget();
         if(lName_times == "up") {
             sort(1, 4, "increase");
             change("staff", 1, "increase");
@@ -225,6 +268,7 @@ $(document).ready(function() {
     });
 //按照lastest checkin来进行排序
     $("#staff thead tr:eq(0) th:eq(2)").click(function() {
+        reget();
         if(check_times == "up") {
             sort(1, 5, "increase");
             change("staff", 2, "increase");
